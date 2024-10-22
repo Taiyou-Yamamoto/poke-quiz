b6e24916f7bec8c116f4f4f54114e88fa50b0c1b
山本　太陽 <yamamototaiyou.811@gmail.com>
2024-10-22 15:31:10 +0900

    スタート画面

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,27 @@ const config: Config = {
       fontFamily: {
         PokeFont: ['Pokemon Hollow', 'sans-serif'],
       },
+      keyframes: {
+        chime: {
+          '0%': { transform: 'rotate(0deg)' },
+          '10%': { transform: 'rotate(5deg)' },
+          '20%': { transform: 'rotate(-5deg)' },
+          '30%': { transform: 'rotate(2deg)' },
+          '40%': { transform: 'rotate(-2deg)' },
+          '45%': { transform: 'rotate(0deg)' },
+          '50%': { transform: 'rotate(1deg)' },
+          '55%': { transform: 'rotate(1deg)' },
+          '60%': { transform: 'rotate(0.5deg)' },
+          '65%': { transform: 'rotate(-0.5deg)' },
+          '70%': { transform: 'rotate(0.2deg)' },
+          '75%': { transform: 'rotate(-0.2deg)' },
+          '78%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(0deg)' },
+        },
+      },
+      animation: {
+        chime: 'chime 3.5s ease-in-out infinite', // 2秒ごとに無限ループ
+      },
     },
   },
   plugins: [],
