813a2d5e644905e92cf83767f6373991c4b539ce
山本　太陽 <yamamototaiyou.811@gmail.com>
2024-10-27 23:42:28 +0900

    domainsをremoteRatternsへ

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    domains: ['raw.githubusercontent.com'],
+    remotePatterns: [
+      {
+        protocol: 'https', // プロトコル（http, https）
+        hostname: 'raw.githubusercontent.com', // 許可するドメイン
+        pathname: '/PokeAPI/sprites/master/sprites/pokemon/**', // 許可するパス（ワイルドカード `**` を使用）
+      },
+    ],
   },
 };
 
