4cacc60879a3021a85a3043370dbc33cc2f947e8
山本　太陽 <yamamototaiyou.811@gmail.com>
2024-10-31 18:28:10 +0900

    画像を変化するように

--- features/quiz/components/QuizImage.tsx
+++ features/quiz/components/QuizImage.tsx
@@ -1,17 +1,11 @@
+import { PokemonImage } from '@/app/type';
 import Image from 'next/image';
 import React from 'react';
 
-const QuizImage = () => {
+const QuizImage = ({ image }: PokemonImage) => {
   return (
-    <div className='my-10 p-9 bg-white border-8 rounded shadow-2xl'>
-      <Image
-        src={
-          'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10072.png'
-        }
-        width={120}
-        height={120}
-        alt={'1'}
-      />
+    <div className=''>
+      <Image src={image} width={120} height={120} alt={'1'} priority />
     </div>
   );
 };
