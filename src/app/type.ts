d5bf7e973a45330ef4a74659ed6e9a1d092ab4dc
山本　太陽 <yamamototaiyou.811@gmail.com>
2024-11-01 22:18:10 +0900

    新しいtype追加

--- src/app/type.ts
+++ src/app/type.ts
@@ -2,3 +2,11 @@ export type Pokemon = {
   name: string;
   url: string;
 };
+
+export type PokemonImage = {
+  image: string;
+};
+export type PokemonName = {
+  PokemonName: string;
+  setScore: React.Dispatch<React.SetStateAction<number>>;
+};
