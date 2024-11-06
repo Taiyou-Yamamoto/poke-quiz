41d5ef74cd8bd995cacab1359127a1d704f152e2
山本　太陽 <yamamototaiyou.811@gmail.com>
2024-11-06 18:27:33 +0900

    検索パラメータ変更処理

--- features/quiz/components/QuizMain.tsx
+++ features/quiz/components/QuizMain.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import QuizImage from './QuizImage';
 import Input from './Input';
 import { quizArrayProps } from '@/app/type';
@@ -15,22 +15,43 @@ const QuizMain = ({ quizArray, detailArray }: quizArrayProps) => {
     router.push('/');
     router.refresh();
   };
+
+  useEffect(() => {
+    if (count > 9) {
+      // 検索パラメータを変更しBGMを変更
+      window.history.replaceState(null, '', '/result');
+    }
+  }, [count]);
   console.log('クイズ', quizArray);
   console.log('詳細', detailArray);
   return (
     <div className='bg-red-300 min-h-screen w-full'>
       {count > 9 ? (
-        <div className='flex justify-center'>
-          <h1>{score}</h1>
-          <form onSubmit={goToHome}>
-            <button type='submit'>戻る</button>
+        <div className='h-full w-full flex flex-col justify-center items-center mt-10 bg-red-300'>
+          <h1 className='font-PokeGB text-3xl text-white font-extrabold gray-shadow'>
+            {score}/10
+          </h1>
+          <div></div>
+          <form onSubmit={goToHome} className=''>
+            <button
+              type='submit'
+              className='bg-yellow-300 gray-shadow return-button max-w-[150px] font-extrabold font-PokeGB text-white py-2 px-4 rounded-md border-solid border-4 mt-4 ring-4 shadow-2xl active:shadow-none'
+            >
+              戻る
+            </button>
           </form>
         </div>
       ) : (
-        <div className='bg-red-300 min-h-screen w-full flex flex-col justify-center'>
-          <h1 className='font-PokeGB text-3xl text-white font-extrabold my-14 gray-shadow'>
-            {count}/10 このポケモンの名前は？
-          </h1>
+        <div className='bg-red-300 min-h-screen w-full flex flex-col justify-center items-center'>
+          <div>
+            <h1 className='font-PokeGB text-3xl text-white font-extrabold gray-shadow items-center'>
+              {count + 1}/10
+            </h1>
+            <h2 className='font-PokeGB text-3xl text-white font-extrabold my-14 gray-shadow'>
+              このポケモンの名前は？
+            </h2>
+          </div>
+
           <QuizImage image={quizArray[count].image} />
           <Input
             PokemonName={quizArray[count].name}
