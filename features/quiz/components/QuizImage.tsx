3b5bc100f1e1faa998315d21ae859328fec68de6
山本　太陽 <yamamototaiyou.811@gmail.com>
2024-11-10 10:44:26 +0900

    keyの追加

--- features/quiz/components/QuizImage.tsx
+++ features/quiz/components/QuizImage.tsx
@@ -5,7 +5,14 @@ import React from 'react';
 const QuizImage = ({ image }: PokemonImage) => {
   return (
     <div className='w-72 h-72  border-4 border-yellow-400 bg-white rounded-md m-10 shadow'>
-      <Image src={image} width={120} height={120} alt={'1'} priority />
+      <Image
+        src={image}
+        width={120}
+        height={120}
+        alt={'1'}
+        key={image}
+        priority
+      />
     </div>
   );
 };
