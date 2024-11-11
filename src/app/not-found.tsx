0bc42aa191f3a4a4921447e92ab99d7acb4f26f6
山本　太陽 <yamamototaiyou.811@gmail.com>
2024-11-11 13:12:28 +0900

    not-found追加

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,8 +3,14 @@ import React from 'react';
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>NotFound</h1>
+    <div className='min-h-screen flex flex-col items-center justify-center'>
+      <h1 className='text-2xl font-extrabold'>ページが見つからないよ...</h1>
+      <Link
+        href='/'
+        className='text-lg bg-yellow-400 hover:bg-yellow-500 gray-shadow max-w-[150px] font-extrabold font-PokeGB text-white py-2 px-4 rounded-md border-solid border-4 mt-4 ring-4 hover:ring-blue-500 shadow-2xl hover:shadow-none transition-all duration-300'
+      >
+        ホームへ戻る
+      </Link>
     </div>
   );
 };
