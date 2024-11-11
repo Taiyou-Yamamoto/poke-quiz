654d5c7da2b8c37d196f078eb4fb9fe59f1c9198
山本　太陽 <yamamototaiyou.811@gmail.com>
2024-11-11 12:56:09 +0900

    not-found追記

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>NotFound</h1>
+    </div>
+  );
+};
+
+export default NotFound;
