d78c3e3bb1c7608af835e70d16a3d7909c388635
山本　太陽 <yamamototaiyou.811@gmail.com>
2024-11-12 17:21:29 +0900

    ボタン調整

--- features/quiz/components/QuizMain.tsx
+++ features/quiz/components/QuizMain.tsx
@@ -8,13 +8,22 @@ import Image from 'next/image';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircle } from '@fortawesome/free-regular-svg-icons';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faXTwitter } from '@fortawesome/free-brands-svg-icons';
+import XPost from './XPost';
 
 const QuizMain = ({ quizArray, detailArray }: quizArrayProps) => {
   const router = useRouter();
-  const [count, setCount] = useState<number>(0);
+  const [count, setCount] = useState<number>(10);
   const [score, setScore] = useState<number>(0);
   const [yourResult, setYourResult] = useState<string[]>([]);
 
+  const oneMore = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setScore(0);
+    setCount(0);
+    router.push('/quiz');
+    router.refresh();
+  };
   const goToHome = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setScore(0);
@@ -36,13 +45,14 @@ const QuizMain = ({ quizArray, detailArray }: quizArrayProps) => {
     if (count > 9) {
       const resultUrl = score > 5 ? '/result_high' : '/result_low';
       // 検索パラメータを変更しBGMを変更
-      window.history.replaceState(null, '', resultUrl);
+      // window.history.replaceState(null, '', resultUrl);
     }
   }, [count, score]);
 
   console.log('クイズ', quizArray);
   console.log('詳細', detailArray);
   console.log('yourResult', yourResult);
+  console.log('router', router);
 
   return (
     <div className='bg-red-300 w-full'>
@@ -56,13 +66,15 @@ const QuizMain = ({ quizArray, detailArray }: quizArrayProps) => {
             <div className='font-PokeGB text-3xl text-white font-extrabold gray-shadow'>
               {resultMessage}
             </div>
-            <table className=''>
+            <table className='mt-10'>
               <thead>
                 <tr>
-                  <th></th>
-                  <th colSpan={2}>答え</th>
-                  <th colSpan={2}>あなたの回答</th>
-                  <th></th>
+                  <th colSpan={2} className='mr-10'>
+                    答え
+                  </th>
+                  <th colSpan={2} className='ml-10'>
+                    あなたの回答
+                  </th>
                 </tr>
               </thead>
               <tbody>
@@ -79,12 +91,12 @@ const QuizMain = ({ quizArray, detailArray }: quizArrayProps) => {
                         />
                       </td>
                       <td>
-                        <h3 className='text-white gray-shadow font-extrabold text-2xl'>
+                        <h3 className='mr-5 text-white gray-shadow font-extrabold text-2xl'>
                           {pokemon.name}
                         </h3>
                       </td>
                       <td>
-                        <h3 className='text-white gray-shadow  font-extrabold text-2xl'>
+                        <h3 className='min-w-36 text-white gray-shadow  font-extrabold text-2xl'>
                           {yourResult[index]}
                         </h3>
                       </td>
@@ -107,14 +119,29 @@ const QuizMain = ({ quizArray, detailArray }: quizArrayProps) => {
                 })}
               </tbody>
             </table>
-            <form onSubmit={goToHome} className=''>
-              <button
-                type='submit'
-                className='bg-yellow-300 gray-shadow return-button max-w-[150px] font-extrabold font-PokeGB text-white py-2 px-4 rounded-md border-solid border-4 mt-4 ring-4 shadow-2xl active:shadow-none'
-              >
-                戻る
-              </button>
-            </form>
+
+            <div className='flex flex-col relative'>
+              <form onSubmit={oneMore}>
+                <button
+                  type='submit'
+                  className='w-[10rem] my-5 bg-yellow-400 hover:bg-yellow-500 gray-shadow max-w-[150px] font-extrabold font-PokeGB text-white py-2 px-4 rounded-md border-solid border-4 ring-4 hover:ring-blue-500 shadow-2xl hover:shadow-none transition-all duration-300'
+                >
+                  再挑戦する
+                </button>
+              </form>
+
+              <form onSubmit={goToHome}>
+                <button
+                  type='submit'
+                  className='w-[10rem] bg-yellow-400 hover:bg-yellow-500 gray-shadow max-w-[150px] font-extrabold font-PokeGB text-white py-2 px-4 rounded-md border-solid border-4 ring-4 hover:ring-blue-500 shadow-2xl hover:shadow-none transition-all duration-300'
+                >
+                  ホームへ戻る
+                </button>
+              </form>
+              <div className='absolute bottom-0 left-60'>
+                <XPost />
+              </div>
+            </div>
           </div>
         ) : (
           <div className=' min-h-screen w-full flex flex-col justify-center items-center'>
