188ecffbf95b68d1664d4f8a685ad85f4f46832e
山本　太陽 <yamamototaiyou.811@gmail.com>
2024-12-29 14:47:40 +0900

    axios

---
+++ src/app/utils/axiosHandle.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://127.0.0.1:8000',
+});
+
+export const postScore = async (calculatedScore: number, quiz_id: number) => {
+  try {
+    const response = await api.post('/post', {
+      calculatedScore: calculatedScore,
+      quiz_id: quiz_id,
+    });
+
+    console.log('Response:', response.data);
+  } catch (error) {
+    console.log(error);
+  }
+};
