09897bb6427cc24774af5a7db9273d18ac07f8d4
山本　太陽 <yamamototaiyou.811@gmail.com>
2025-01-01 00:19:00 +0900

    分割

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import Image from 'next/image';
 import {
   getAllPokemonNameAndUrl,
   getTwoHundredPokemonDetailDate,
+  makeFiveImageArray,
 } from './utils/dataHandle';
 import MainContent from '../../features/Home/components/MainContent';
+import Background from '../../features/Home/components/Background';
 
 export default async function Home() {
   const POKEMON_API_URL = process.env.NEXT_PUBLIC_POKEMON_API_URL;
@@ -29,26 +31,12 @@ export default async function Home() {
   }
   const shuffledTwoHundredArray = await shuffledTwoHundredData.json();
   const imageArray = getTwoHundredPokemonDetailDate(shuffledTwoHundredArray);
-
+  const fiveImages = makeFiveImageArray(imageArray);
+  console.log(fiveImages);
   return (
     <>
-      <div className='w-full h-screen  bg-red-300 fixed'>
-        <div className='absolute inset-0 z-0 opacity-85 flex flex-wrap'>
-          {imageArray.map((pokemonUrl: string, index: number) => {
-            return (
-              <div key={index}>
-                <Image
-                  className='w-auto h-auto'
-                  src={pokemonUrl}
-                  width={300}
-                  height={300}
-                  alt={String(index)}
-                  priority
-                />
-              </div>
-            );
-          })}
-        </div>
+      <div className='w-full h-screen  bg-red-300 '>
+        <Background fiveImages={fiveImages} />
         <MainContent />
       </div>
     </>
