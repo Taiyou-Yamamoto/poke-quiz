489fc2737a028fbcbf1f2efbd6238d3601464d2f
山本　太陽 <yamamototaiyou.811@gmail.com>
2024-12-31 21:53:38 +0900

    ランキング表完成

--- features/Home/components/MainContent.tsx
+++ features/Home/components/MainContent.tsx
@@ -3,27 +3,53 @@ import { getScore } from '@/app/utils/axiosHandle';
 import Image from 'next/image';
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
+import { useTheme } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+import { a11yProps, TabPanel } from '@/app/utils/MaterialUI';
+import Ranking from './Ranking';
+import { RankData } from '@/app/type';
 
 const MainContent = () => {
   const [toggle, SetToggle] = useState<boolean>(false);
-  const [rankData, SetRankData] = useState<number[]>([]);
+  const [rankData, SetRankData] = useState<RankData>({
+    quiz1: [],
+    quiz2: [],
+    quiz3: [],
+  });
+  const theme = useTheme();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
   const toggleSwitch = () => {
     SetToggle(!toggle);
+  };
+
+  const makeFullArray = (array: any) => {
+    while (array.length < 10) {
+      array.push({ score: 0 });
+    }
+    return array;
   };
 
   useEffect(() => {
     const fetchData = async () => {
       const res = await getScore();
-      const array = [];
-      for (const data of res.quiz1) {
-        array.push(data.score);
-      }
 
-      while (array.length < 10) {
-        array.push(null);
-      }
-      SetRankData(array);
-      console.log(rankData);
+      // 空配列チェックとデフォルト値設定
+      const updatedRes = {
+        quiz1: res.quiz1.length <= 10 ? makeFullArray(res.quiz1) : res.quiz1,
+        quiz2: res.quiz2.length <= 10 ? makeFullArray(res.quiz2) : res.quiz2,
+        quiz3: res.quiz3.length <= 10 ? makeFullArray(res.quiz3) : res.quiz3,
+      };
+
+      console.log('updatedRes', updatedRes);
+      SetRankData(updatedRes);
     };
 
     fetchData();
@@ -31,8 +57,9 @@ const MainContent = () => {
   }, []);
 
   useEffect(() => {
-    console.log('Updated rankData:', rankData); // rankData が更新された後に出力
+    console.log(rankData);
   }, [rankData]);
+
   return (
     <div className='h-full w-full z-50'>
       <div className='relative z-50 start top-14 left-3/4'>
@@ -42,26 +69,79 @@ const MainContent = () => {
       </div>
 
       {toggle ? (
-        <div className='relative z-50 h-4/6 w-[35rem] flex justify-center items-center mx-auto mt-28 bg-white bg-opacity-80 rounded-3xl shadow-xl border-4 border-sky-400'>
-          <div className='font-DotJP h-full w-4/6 marker:text-4xl font-extrabold gray-shadow overscroll-x-auto'>
-            <div className='my-10 text-3xl'>モンスターボールレベル</div>
-            <ul className='flex-col text-2xl items-center justify-center'>
-              {rankData.map((score, index) => (
-                <li
-                  key={index}
-                  className='flex items-center justify-between w-full py-1'
+        <>
+          <div className='relative flex justify-center items-center mt-28'>
+            <Box
+              sx={{
+                maxWidth: '500px',
+                width: '100%',
+                boxShadow: 10,
+                bgcolor: 'rgba(255, 255, 255, 0.9)',
+                borderRadius: 4,
+              }}
+            >
+              <AppBar
+                position='static'
+                sx={{
+                  borderTopLeftRadius: 16, // 左上の角を丸くする
+                  borderTopRightRadius: 16, //
+                  bgcolor: 'rgba(255, 255, 255, 0)',
+                  fontWeight: 500,
+                }}
+              >
+                <Tabs
+                  value={value}
+                  onChange={handleChange}
+                  indicatorColor='secondary'
+                  textColor='inherit'
+                  variant='fullWidth'
+                  aria-label='full width tabs example'
                 >
-                  <span className='flex-shrink-0 w-[5rem] text-right'>
-                    {index + 1}位:
-                  </span>
-                  <span className='ml-3 text-center w-full'>
-                    {score ? score : '---------------------'}
-                  </span>
-                </li>
-              ))}
-            </ul>
+                  <Tab
+                    label='モンスター'
+                    {...a11yProps(0)}
+                    sx={{
+                      borderTopLeftRadius: 16,
+                      bgcolor: 'rgb(246, 63, 63)',
+                      fontWeight: 'bold',
+                      fontSize: '1rem',
+                    }}
+                  />
+                  <Tab
+                    label='スーパー'
+                    {...a11yProps(1)}
+                    sx={{
+                      bgcolor: '#38a4f1',
+                      fontWeight: 'bold',
+                      fontSize: '1rem',
+                    }}
+                    className={'gray-shadow'}
+                  />
+                  <Tab
+                    label='ハイパー'
+                    {...a11yProps(2)}
+                    sx={{
+                      borderTopRightRadius: 16,
+                      bgcolor: 'rgba(255, 224, 50, 0.931)',
+                      fontWeight: 'bold',
+                      fontSize: '1rem',
+                    }}
+                    className={'gray-shadow'}
+                  />
+                </Tabs>
+              </AppBar>
+              <TabPanel value={value} index={0} dir={theme.direction}>
+                <Ranking rankData={rankData.quiz1} quizId={1} />
+              </TabPanel>
+              <TabPanel value={value} index={1} dir={theme.direction}>
+                <Ranking rankData={rankData.quiz2} quizId={2} />
+              </TabPanel>
+              <TabPanel value={value} index={2} dir={theme.direction}>
+                <Ranking rankData={rankData.quiz3} quizId={3} />
+              </TabPanel>
+            </Box>
           </div>
-        </div>
+        </>
       ) : (
         <>
           {' '}
---
+++ features/Home/components/Ranking.tsx
@@ -0,0 +1,44 @@
+import { RankingProps } from '@/app/type';
+import React, { useEffect } from 'react';
+
+const Ranking = ({ rankData, quizId }: RankingProps) => {
+  useEffect(() => {
+    console.log('rankData', rankData);
+  }, [rankData]);
+  return (
+    <div className='font-DotJP h-full w-full my-3 text-4xl font-extrabold '>
+      <div className='mb-5 text-3xl mx-auto text-center gray-shadow'>
+        {quizId === 1
+          ? 'モンスターレベル'
+          : quizId === 2
+          ? 'スーパーレベル'
+          : 'ハイパーレベル'}
+      </div>
+      <ul className='flex-col text-3xl items-center justify-center'>
+        {rankData.map((item, index) => (
+          <li
+            key={index}
+            className={
+              index == 0
+                ? 'flex items-center justify-between w-full py-1 !font-black bg-gradient-to-t from-[#e4b255] to-[#ffe138] shine-text gold-shadow bg-clip-text text-transparent'
+                : index == 1
+                ? 'flex items-center justify-between w-full py-1 !font-black bg-gradient-to-t from-[#888888] to-[#ebebeb]  bg-clip-text text-transparent'
+                : index == 2
+                ? 'flex items-center justify-between w-full py-1 !font-black bg-gradient-to-t from-[#a48255] to-[#ff9d5f]  bg-clip-text text-transparent'
+                : 'flex items-center justify-between w-full py-1 !font-black  gray-shadow text-black'
+            }
+          >
+            <span className={'flex-shrink-0 w-[5rem] text-right'}>
+              {index + 1}位:
+            </span>
+            <span className='ml-3 text-center w-full'>
+              {item.score ? item.score : '------------------'}
+            </span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Ranking;
