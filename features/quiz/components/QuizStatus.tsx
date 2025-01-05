72f70f4f78022544cfc3e4993f9f6b106c5bff23
山本　太陽 <yamamototaiyou.811@gmail.com>
2025-01-05 17:48:33 +0900

    sutatus部分をコンポーネント化

---
+++ features/quiz/components/QuizStatus.tsx
@@ -0,0 +1,38 @@
+import { QuizStatusProps } from '@/app/type';
+import React from 'react';
+
+const QuizStatus = ({ second, count, quiz_id }: QuizStatusProps) => {
+  return (
+    <>
+      <div className='h-[7rem] text-2xl text-white gray-shadow'>
+        残り
+        <span
+          className={second < 11 ? 'text-red-600 font-PokeGB' : 'font-PokeGB'}
+        >
+          {second}
+        </span>
+        秒
+      </div>
+      <div className='flex flex-col gap-4 justify-center items-center'>
+        <h1 className='flex font-PokeGB text-3xl text-white font-extrabold gray-shadow '>
+          {count + 1}問目
+        </h1>
+        {quiz_id == 1 ? (
+          <h2 className='font-PokeHira text-3xl text-white  font-extrabold gray-shadow'>
+            このポケモンの名前は？
+          </h2>
+        ) : quiz_id == 2 ? (
+          <h2 className='font-PokeHira text-3xl text-white  font-extrabold gray-shadow'>
+            これはどのポケモンの鳴き声？
+          </h2>
+        ) : (
+          <h2 className='font-PokeHira text-3xl text-white  font-extrabold gray-shadow'>
+            これはどのポケモンの図鑑説明？
+          </h2>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default QuizStatus;
