e3768297f825a297adea48adf65290da90b05212
山本　太陽 <yamamototaiyou.811@gmail.com>
2025-01-05 18:50:21 +0900

    RetryAndHomeと一体化

--- features/quiz/components/XPost.tsx
+++ features/quiz/components/XPost.tsx
@@ -9,14 +9,16 @@ const XPost = ({ calculatedScore }: { calculatedScore: number }) => {
   );
 
   return (
-    <a
-      className='flex flex-row bg-black py-2 px-4 rounded-3xl justify-center items-center shadow-2xl'
-      href={`https://twitter.com/intent/tweet?text=${tweetContent}&hashtags=PokeQuiz`}
-      data-size='large'
-    >
-      <FontAwesomeIcon icon={faXTwitter} className='text-white mr-1' />
-      <h1 className='text-white font-extrabold'>POST</h1>
-    </a>
+    <div className='absolute bottom-0 left-60'>
+      <a
+        className='flex flex-row bg-black py-2 px-4 rounded-3xl justify-center items-center shadow-2xl'
+        href={`https://twitter.com/intent/tweet?text=${tweetContent}&hashtags=PokeQuiz`}
+        data-size='large'
+      >
+        <FontAwesomeIcon icon={faXTwitter} className='text-white mr-1' />
+        <h1 className='text-white font-extrabold'>POST</h1>
+      </a>
+    </div>
   );
 };
 
