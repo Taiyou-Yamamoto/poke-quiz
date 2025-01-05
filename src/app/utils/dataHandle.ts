e33b3ddfca5472e9f8dbabd5593f13bc896b9e22
山本　太陽 <yamamototaiyou.811@gmail.com>
2025-01-05 22:43:14 +0900

    データを小さく

--- src/app/utils/dataHandle.ts
+++ src/app/utils/dataHandle.ts
@@ -24,7 +24,7 @@ export const getAllPokemonNameAndUrl = async (
 
 // ランダムな1302匹分のurlを200匹に絞り,fetchし、配列にして返す
 export const getTwoHandredAllPokemonDetail = async (array: Pokemon[]) => {
-  const twentyPokemo = array.slice(0, 199);
+  const twentyPokemo = array.slice(0, 60);
   const detailDatas = twentyPokemo.map(async (pokemon) => {
     const res = await fetch(pokemon.url);
     return res.json();
