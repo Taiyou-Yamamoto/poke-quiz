c855d96a7096aa37f38d8e8a42141922ff961e05
山本　太陽 <yamamototaiyou.811@gmail.com>
2025-01-08 23:01:46 +0900

    テストコード削除

--- src/app/api/home/route.ts
+++ src/app/api/home/route.ts
@@ -2,15 +2,6 @@ import { createRandomPokemonData, getOnlyImage } from '@/app/utils/dataHandle';
 import { NextResponse } from 'next/server';
 import { getCustomPokemonData } from '../../utils/dataHandle';
 
-// export async function GET() {
-//   try {
-//     return NextResponse.json({ message: 'GET is working!' });
-//   } catch (error) {
-//     console.error('Error in API:', error);
-//     return NextResponse.json({ error: 'Something went wrong' });
-//   }
-// }
-
 export async function POST(request: Request) {
   try {
     const req = await request.json();
