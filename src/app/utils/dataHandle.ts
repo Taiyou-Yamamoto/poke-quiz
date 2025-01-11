100f06a15f83028a4d1becc84a95961fcf712d3e
山本　太陽 <yamamototaiyou.811@gmail.com>
2025-01-12 04:59:44 +0900

    データ取得関数の完成

--- src/app/utils/dataHandle.ts
+++ src/app/utils/dataHandle.ts
@@ -1,4 +1,4 @@
-import { Pokemon } from '../type';
+import { Pokemon, PokemonData } from '../type';
 import { excludedUrls } from './exclidedUrls';
 
 // 1302匹ぶんのnameとurlをSSGで取得
@@ -42,7 +42,7 @@ export const createRandomPokemonData = (array: any[]): any[] => {
 };
 
 //ポケモンの詳細データを取得する関数
-export const fetchPokemonDetails = async (url: string): Promise<any | null> => {
+export const fetchPokemonDetails = async (url: string): Promise<any> => {
   try {
     const res = await fetch(url, { cache: 'force-cache' });
     if (res.ok) {
@@ -74,8 +74,13 @@ export const getCustomPokemonData = async (
 
   try {
     for (const pokemon of randomArray) {
-      const data = await ImageFilter(pokemon.url, callback);
-      if (data) {
+      const data: PokemonData = await ImageFilter(pokemon.url, callback);
+      if (
+        data &&
+        Object.values(data).every((value: string | null) => value !== null)
+      ) {
+        // if (data) {
+        // console.log(Object.values(data));
         DataArray.push(data);
       }
       if (DataArray.length >= count) {
@@ -88,8 +93,8 @@ export const getCustomPokemonData = async (
 
   return DataArray;
 };
-
-export const getSpeciesData = async (data: any) => {
+// 各個体のSpeciesDataを取得
+export const getSpeciesData = async (data: any): Promise<any> => {
   const pokemonDetailResponse: any = await fetch(data.species.url, {
     cache: 'no-store',
   });
@@ -97,151 +102,97 @@ export const getSpeciesData = async (data: any) => {
   return await pokemonDetailResponse.json();
 };
 
-export const getJPName = async (speciesData: any[]) => {
-  const pokemonNames = await getSpeciesData(speciesData);
-
-  const PokemonJPName = pokemonNames.names.find(
+export const getJPName = async (speciesData: any): Promise<string> => {
+  const PokemonJPName = speciesData.names.find(
     (pokemonData: any) => pokemonData.language.name === 'ja-Hrkt'
   );
-  return PokemonJPName;
+  return PokemonJPName.name;
 };
 
-export const getText = async (speciesData: any[]) => {
-  const pokemonNames = await getSpeciesData(speciesData);
-
+export const getText = async (speciesData: any): Promise<string | null> => {
   //flavor_text_entriesがない場合はnullを返す
-  if (!pokemonNames.flavor_text_entries) {
+  if (!speciesData.flavor_text_entries) {
     return null;
   }
 
   // 日本語のテキストを取得
-  const JPEntry = pokemonNames.flavor_text_entries.find(
+  const JPName = speciesData.flavor_text_entries.find(
     (pokemonData: any) => pokemonData.language.name === 'ja'
   );
 
   // 見つからなければ null を返す
-  if (!JPEntry) {
+  if (!JPName) {
     return null;
   }
 
   // 日本語テキストが見つかった場合のみデータを返す
-  return JPEntry.flavor_text;
+  return JPName.flavor_text;
 };
 
-export const getCries = async (data: any) => {
+export const getCries = (data: any): string => {
   return data.cries.latest;
 };
+
 /********  以下の関数がgetCustomPokemonDataのcallbackとして使用  *********/
 
 //画像データのみ取得
-export const getOnlyImage = (data: any) => {
+export const getImage = (data: any): string => {
   return data.sprites.front_default;
 };
-
-// 画像データと日本語名を取得
-
-// export const getImage_JPName = async (data: any) => {
-//   const pokemonNameData: any = await fetch(data.species.url, {
-//     cache: 'no-store',
-//   });
-
-//   const pokemonNames = await pokemonNameData.json();
-
-//   let count: number = 0;
-//   while (pokemonNames.names[count].language.name !== 'ja-Hrkt') {
-//     count++;
-//   }
-//   const pokemonJpName = pokemonNames.names[count].name;
-
-//   return {
-//     image: getOnlyImage(data),
-//     name: pokemonJpName,
-//   };
-// };
-export const getImage_JPName = async (data: any) => {
+//画像と日本語名を取得
+export const getImage_JPName = async (
+  data: any
+): Promise<{
+  image: string;
+  name: string;
+}> => {
+  const pokemonImage = getImage(data);
   const pokemonNames = await getSpeciesData(data);
-
-  let count: number = 0;
-  while (pokemonNames.names[count].language.name !== 'ja-Hrkt') {
-    count++;
-  }
-  const pokemonJpName = pokemonNames.names[count].name;
+  const pokemonJPName = await getJPName(pokemonNames);
 
   return {
-    image: getOnlyImage(data),
-    name: pokemonJpName,
+    image: pokemonImage,
+    name: pokemonJPName,
   };
 };
 
 // 画像データと日本語名とテキストを取得
-export const getImage_JPName_Text = async (data: any) => {
+export const getImage_JPName_Text = async (
+  data: any
+): Promise<{
+  image: string;
+  name: string;
+  text: string | null;
+}> => {
+  const pokemonImage = getImage(data);
   const pokemonNames = await getSpeciesData(data);
-  const { image, name } = await getImage_JPName(data);
-
-  //flavor_text_entriesがない場合はnullを返す
-
-  if (!pokemonNames.flavor_text_entries) {
-    return null;
-  }
-
-  // 日本語のテキストを取得
-  const JPEntry = pokemonNames.flavor_text_entries.find(
-    (pokemonData: any) => pokemonData.language.name === 'ja'
-  );
-
-  // 見つからなければ null を返す
-  if (!JPEntry) {
-    return null;
-  }
-
-  // 日本語テキストが見つかった場合のみデータを返す
+  const pokemonJPName = await getJPName(pokemonNames);
+  const pokemonText = await getText(pokemonNames);
   return {
-    image,
-    name,
-    text: JPEntry.flavor_text,
+    image: pokemonImage,
+    name: pokemonJPName,
+    text: pokemonText,
   };
 };
-// export const getImage_JPName_Text = async (data: any) => {
-//   const { image, name } = await getImage_JPName(data);
-//   const pokemonNameData: any = await fetch(data.species.url, {
-//     cache: 'no-store',
-//   });
 
-//   //flavor_text_entriesがない場合はnullを返す
-//   const pokemonNames = await pokemonNameData.json();
-//   if (!pokemonNames.flavor_text_entries) {
-//     return null;
-//   }
-
-//   // 日本語のテキストを取得
-//   const JPEntry = pokemonNames.flavor_text_entries.find(
-//     (pokemonData: any) => pokemonData.language.name === 'ja'
-//   );
-
-//   // 見つからなければ null を返す
-//   if (!JPEntry) {
-//     return null;
-//   }
-
-//   // 日本語テキストが見つかった場合のみデータを返す
-//   return {
-//     image,
-//     name,
-//     text: JPEntry.flavor_text,
-//   };
-// };
-
-// 画像データと日本語名と鳴き声を取得
-export const getImage_JPName_Cries = async (data: any) => {
+// 画像データと日本語名とテキストと鳴き声を取得
+export const getImage_JPName_Cries = async (
+  data: any
+): Promise<{ image: string; name: string; cry: string }> => {
+  const pokemonImage = getImage(data);
+  const pokemonCries = getCries(data);
   const pokemonNames = await getSpeciesData(data);
-  const { image, name } = await getImage_JPName(data);
-  return { image, name, cry: data.cries.latest };
+  const pokemonJPName = await getJPName(pokemonNames);
+  return { image: pokemonImage, name: pokemonJPName, cry: pokemonCries };
 };
 
 /*****************************  ここまで  *******************************/
 
 // 3分ごとにデータをシャッフル
-export const getShuffledFiftyData = async (url: string, array: Pokemon[]) => {
+export const getShuffledFiftyData = async (
+  url: string,
+  array: Pokemon[]
+): Promise<any[]> => {
   const res = await fetch(url, {
     method: 'POST',
     headers: {
@@ -263,7 +214,7 @@ export const getShuffledFiftyData = async (url: string, array: Pokemon[]) => {
 };
 
 // ５つの配列に分割
-export const makeFiveImageArray = (imageArray: string[]) => {
+export const makeFiveImageArray = (imageArray: string[]): string[][] => {
   const array = [];
 
   for (let i = 0; i <= 50; i += 10) {
