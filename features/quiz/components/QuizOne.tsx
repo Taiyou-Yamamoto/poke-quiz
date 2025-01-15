2e5b396c57d1e9de4971ef59e06566edaf1cea5c
山本　太陽 <yamamototaiyou.811@gmail.com>
2025-01-15 10:28:33 +0900

    QuizOne  画像を描写する

---
+++ features/quiz/components/QuizOne.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image';
+import React from 'react';
+
+const QuizOne = ({ image }: { image: string }) => {
+  return (
+    <div className='flex justify-center items-center border-4 border-yellow-400 bg-white rounded-md m-10 shadow w-auto h-auto'>
+      <Image
+        src={image}
+        width={130}
+        height={130}
+        alt={'2'}
+        key={image}
+        priority
+      />
+    </div>
+  );
+};
+
+export default QuizOne;
