aaad4dcade7ec25e6c0fc2a146d10ad262be6bfe
山本　太陽 <yamamototaiyou.811@gmail.com>
2025-01-15 10:26:03 +0900

    blinkは使わないので削除

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -43,11 +43,7 @@ const config: Config = {
           '25%': { transform: 'translateX(0%)' },
           '100%': { transform: 'translateX(0%)' },
         },
-        blink: {
-          '0%': { opacity: '1' },
-          '50%': { opacity: '0' },
-          '100%': { opacity: '1' },
-        },
+
         slide: {
           '0%': { transform: 'translateX(0)' },
           '100%': { transform: 'translateX(-800px)' },
