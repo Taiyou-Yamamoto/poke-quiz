cc54f15fdf0af7e3067231862dc139721e8a9424
山本　太陽 <yamamototaiyou.811@gmail.com>
2025-01-15 10:27:23 +0900

    コメントを削除

--- src/app/quiz2/page.tsx
+++ src/app/quiz2/page.tsx
@@ -2,68 +2,30 @@ import React from 'react';
 import {
   createRandomPokemonData,
   getAllPokemonNameAndUrl,
+  getCustomPokemonData,
+  getImage_JPName_Cries,
 } from '../utils/dataHandle';
-import { excludedUrls } from '../utils/exclidedUrls';
 import QuizMain from '../../../features/quiz/components/QuizMain';
 
-// これを消し
-//
 const Page = async () => {
-  // 修正必須
-  const quizArray = [];
-
-  // const POKEMON_API_URL = process.env.NEXT_PUBLIC_POKEMON_API_URL;
-
-  // 必要
-  // const allPokemonDate = await getAllPokemonNameAndUrl(POKEMON_API_URL!);
-  // const shuffledData = createRandomPokemonData(allPokemonDate);
-
-  // これはquizに必要な分のデータ
-  // const quizData = shuffledData.slice(0, 19);
-
-  // そして20匹分のnameとurlのurlだけをfetchして詳細データを取得
-  // const quizDetailData = quizData.map(async (pokemon) => {
-  //   const res = await fetch(pokemon.url, {
-  //     cache: 'no-store',
-  //   });
-  //   return res.json();
-  // });
-  // 全ての非同期が完了
-  // const detailArray = await Promise.all(quizDetailData);
-  // console.log('detailArray', detailArray);
-
-  // // 以上のデータを元に10匹分のデータを取得する。初めに画像データより10匹を選別
-
-  // for (let i = 0; quizArray.length < 10; ++i) {
-  //   const imageData = detailArray[i].sprites.front_default;
-  //   // const criesData = detailArray[i].cries.latest;
-  //   if (!imageData || excludedUrls.includes(imageData)) {
-  //     continue;
-  //   }
-  //   const pokemonSpecies = await fetch(detailArray[i].species.url);
-  //   const pokemonName = await pokemonSpecies.json();
-  //   const pokemonJpName = pokemonName.names[0].name;
-
-  // console.log('criesData', criesData);
-  // quizArrayはresult画面でも活用
-  // quizArray.push({
-  //   image: imageData,
-  //   name: pokemonJpName,
-  // cries: criesData,
-  // });
-  // }
-
   const quiz_id = 2;
+  const POKEMON_API_URL = process.env.NEXT_PUBLIC_POKEMON_API_URL;
 
-  return (
-    <>
-      {/* <QuizMain
-        quizArray={quizArray}
-        detailArray={detailArray}
-        quiz_id={quiz_id}
-      /> */}
-    </>
+  // 1302匹ぶんのnameとurlをSSGで取得
+  const allPokemonDate = await getAllPokemonNameAndUrl(POKEMON_API_URL!);
+
+  // 昇順だったポケモン詳細配列をランダムに並び替えする配列
+  const shuffledData = createRandomPokemonData(allPokemonDate);
+
+  // 1０匹の日本語名と画像データを取得
+  const Image_JPName = await getCustomPokemonData(
+    shuffledData,
+    10,
+    getImage_JPName_Cries
   );
+
+  const quizArray = Image_JPName;
+  return <>{<QuizMain quizArray={quizArray} quiz_id={quiz_id} />}</>;
 };
 
 export default Page;
