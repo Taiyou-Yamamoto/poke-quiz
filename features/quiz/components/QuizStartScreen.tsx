5eb87f366a79c082d86a9966078360e45e382006
山本　太陽 <yamamototaiyou.811@gmail.com>
2025-01-19 14:41:34 +0900

    テキストを調整

--- features/quiz/components/QuizStartScreen.tsx
+++ features/quiz/components/QuizStartScreen.tsx
@@ -2,22 +2,26 @@ import React from 'react';
 
 const QuizStartScreen = ({
   setQuizStart,
+  quiz_id,
 }: {
   setQuizStart: React.Dispatch<React.SetStateAction<boolean>>;
+  quiz_id: number;
 }) => {
-  const startAction = (e: any) => {
-    if (e.key === ' ') {
-      console.log('hello');
-      setQuizStart(false);
-    }
+  const handleStart = () => {
+    setQuizStart(false);
   };
   return (
-    <div
-      tabIndex={0}
-      className='h-screen w-full flex items-center justify-center'
-      onKeyDown={startAction}
-    >
-      スペースを押してね!
+    <div className='h-screen w-full flex flex-col items-center justify-center gray-shadow'>
+      <h2 className='pb-5 text-2xl text-slate-100'>
+        {quiz_id === 1
+          ? 'ポケモンの画像が出てくるよ。君は何問解けるかな？'
+          : quiz_id === 2
+          ? 'スーパーボール級のお題は鳴き声だ！始める前に音量を調整してね！'
+          : '最後の問題！ポケモン図鑑のテキストを当てよう！'}
+      </h2>
+      <button onClick={handleStart} className='yellow-button'>
+        スタート
+      </button>
     </div>
   );
 };
