5e761aea347a0b2f89f847a0d7b3b35165517bc2
山本　太陽 <yamamototaiyou.811@gmail.com>
2025-01-19 12:05:31 +0900

    スタイルにMUI導入

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -4,6 +4,7 @@ import { usePathname } from 'next/navigation';
 import React, { useContext, useEffect, useRef, useState } from 'react';
 import useSound from 'use-sound';
 import { ControlCriesContext } from './Provider';
+import { ToggleSwitch } from './utils/MaterialUI';
 
 const Header = () => {
   const [toggle, setToggle] = useState<boolean>(false);
@@ -14,9 +15,9 @@ const Header = () => {
   //BGM用
   const audioRef = useRef<HTMLAudioElement>(null);
   //QuizTwoの鳴き声再生用
-  const cryRef = useRef<HTMLAudioElement>(null);
+  // const cryRef = useRef<HTMLAudioElement>(null);
   //Header.tsxとQuizTwoを連携するためのコンテキスト
-  const controlCries = useContext(ControlCriesContext);
+  // const controlCries = useContext(ControlCriesContext);
 
   // BGM用のトグルボタン関数
   const controlToggle = () => {
@@ -29,31 +30,31 @@ const Header = () => {
   };
 
   //鳴き声再生後、再生状態をfalseへ
-  const switchIsPlay = () => {
-    controlCries?.setIsPlaying(false);
-  };
+  // const switchIsPlay = () => {
+  //   controlCries?.setIsPlaying(false);
+  // };
 
   //ここでquiz2の音声をコントロールする
-  useEffect(() => {
-    if (cryRef.current) {
-      cryRef.current.volume = 1;
-      if (controlCries?.cry) {
-        cryRef.current.pause();
-        cryRef.current.src = controlCries.cry;
-      }
-      cryRef.current.play();
+  // useEffect(() => {
+  //   if (cryRef.current) {
+  //     cryRef.current.volume = 1;
+  //     if (controlCries?.cry) {
+  //       cryRef.current.pause();
+  //       cryRef.current.src = controlCries.cry;
+  //     }
+  //     cryRef.current.play();
 
-      if (controlCries?.setIsPlaying) {
-        controlCries.setIsPlaying(true);
-      }
-    }
-  }, [controlCries?.cry]);
+  //     if (controlCries?.setIsPlaying) {
+  //       controlCries.setIsPlaying(true);
+  //     }
+  //   }
+  // }, [controlCries?.cry]);
 
-  useEffect(() => {
-    if (controlCries?.isPlaying) {
-      cryRef.current?.play();
-    }
-  }, [controlCries?.isPlaying]);
+  // useEffect(() => {
+  //   if (controlCries?.isPlaying) {
+  //     cryRef.current?.play();
+  //   }
+  // }, [controlCries?.isPlaying]);
 
   useEffect(() => {
     if (audioRef.current) {
@@ -115,26 +116,16 @@ const Header = () => {
   }, [toggle]);
 
   return (
-    <div className='bg-opacity-0 m-10'>
+    <div className='absolute flex flex-row items-center justify-center z-50 top-12 left-14 bg-orange-400 rounded-2xl shadow'>
       {/* BGM用 */}
       <audio
         ref={audioRef}
         loop={!(pathname === '/result_high' || pathname === '/result_low')}
       />
-      {/* QuizTwoの鳴き声再生用 */}
-      <audio ref={cryRef} onEnded={switchIsPlay} />
-      <label className='inline-flex items-center cursor-pointer bg-orange-400 px-2 py-4 rounded shadow'>
-        <input
-          type='checkbox'
-          className='sr-only peer'
-          checked={toggle}
-          onChange={controlToggle}
-        />
-        <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-        <span className='ms-3 font-semibold  text-white dark:text-gray-300 gray-shadow'>
-          BGM
-        </span>
-      </label>
+      <ToggleSwitch checked={toggle} onChange={controlToggle} />
+      <span className='w-full mr-3 font-semibold  text-white dark:text-gray-300 gray-shadow'>
+        BGM
+      </span>
     </div>
   );
 };
