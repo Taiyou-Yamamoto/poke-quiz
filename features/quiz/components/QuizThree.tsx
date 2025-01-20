f961190c036d300eecff9335eaffa7394b28569e
山本　太陽 <yamamototaiyou.811@gmail.com>
2025-01-20 12:43:21 +0900

    widthの調整

--- features/quiz/components/QuizThree.tsx
+++ features/quiz/components/QuizThree.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const QuizThree = ({ text }: { text: string }) => {
   return (
-    <h1 className=' w-5/6 h-auto text-3xl  m-10 p-5 flex justify-center items-center bg-white border-4 border-yellow-400  rounded-md shadow  break-words'>
+    <h1 className='max-w-[40rem] h-auto text-3xl  m-10 p-5 flex justify-center items-center bg-white border-4 border-yellow-400  rounded-md shadow  break-words'>
       {text}
     </h1>
   );
