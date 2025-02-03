96b488be2ec67dad975c559f787e0a7160c59649
山本　太陽 <yamamototaiyou.811@gmail.com>
2025-02-03 20:27:09 +0900

    fix useEffect to correct calculatedScore

--- features/quiz/components/Score.tsx
+++ features/quiz/components/Score.tsx
@@ -3,24 +3,28 @@ import { scoreCalculateProps } from '@/app/utils/type';
 import React, { useEffect } from 'react';
 
 const Score = ({
+  count,
   score,
   second,
   calculatedScore,
   setCalculatedScore,
 }: scoreCalculateProps) => {
   useEffect(() => {
-    let calculatedData;
-    if (second === 0) {
-      calculatedData = score * 1.1 * Math.pow(score, 0.7) * 10 * 0.95;
-    } else {
-      calculatedData = score * 1.1 * (second * Math.pow(score, 0.7)) * 10;
-    }
-    setCalculatedScore(Math.floor(calculatedData));
-    console.log('calculatedScore', calculatedScore);
+    if (count > 9 || second === 0)
+      if (second === 0) {
+        setCalculatedScore(
+          Math.floor(score * 1.1 * Math.pow(score, 0.7) * 10 * 0.95)
+        );
+      } else {
+        setCalculatedScore(
+          Math.floor(score * 1.1 * (second * Math.pow(score, 0.7)) * 10)
+        );
+      }
+
     console.log('second', second);
     console.log('score', score);
-    console.log(calculatedData);
-  }, [second, score]);
+  }, [second, count]);
+
   return (
     <div>
       <div className='flex font-DotJP text-white text-3xl font-extrabold gray-shadow justify-center items-center mb-5'>
